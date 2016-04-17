661bfd801b7dd39088b4edc94b77156a6817b00d
Nicholas "LB" Braden <LB@LB-Stuff.com>
2016-04-16 20:42:49 -0500

    Move implementation to source file

--- src/json.cpp
+++ src/json.cpp
@@ -4,6 +4,152 @@ namespace LB
 {
 	namespace json
 	{
+		struct value::base
+		{
+			virtual ~base() noexcept = default;
+
+			friend bool operator==(base const &a, base const &b) noexcept
+			{
+				return a.equals(b);
+			}
+			friend bool operator<(base const &a, base const &b) noexcept
+			{
+				return a.less(b);
+			}
+
+			virtual bool equals(base const &) const noexcept = 0;
+			virtual bool less(base const &) const noexcept = 0;
+			virtual base *clone() const noexcept = 0;
+		};
+		template<typename T>
+		struct value::wrap final
+		: base
+		{
+			T v;
+			wrap() noexcept
+			: v{}
+			{
+			}
+			wrap(T const &t) noexcept
+			: v(t)
+			{
+			}
+			wrap(T &&t) noexcept
+			: v(t)
+			{
+			}
+			wrap(wrap const &w) noexcept
+			: v(w.v)
+			{
+			}
+			wrap(wrap &&) noexcept = default;
+			wrap &operator=(wrap const &) = delete;
+			wrap &operator=(wrap &&) = delete;
+
+			virtual bool equals(base const &p) const noexcept override
+			{
+				if(auto w = dynamic_cast<wrap const *>(&p))
+				{
+					return v == w->v;
+				}
+				return false;
+			}
+			virtual bool less(base const &p) const noexcept override
+			{
+				if(auto w = dynamic_cast<wrap const *>(&p))
+				{
+					return v < w->v;
+				}
+				return false;
+			}
+			virtual wrap *clone() const noexcept override
+			{
+				return new wrap{*this};
+			}
+		};
+
+		value::value(std::nullptr_t) noexcept
+		: t{type::null}
+		, p{nullptr}
+		{
+		}
+		value::value(bool v)
+		: t{type::boolean}
+		, p{new wrap<bool>{v}}
+		{
+		}
+		value::value(integer v)
+		: t{type::integer}
+		, p{new wrap<integer>{v}}
+		{
+		}
+		value::value(real v)
+		: t{type::real}
+		, p{new wrap<real>{v}}
+		{
+		}
+		value::value(string const &v)
+		: t{type::string}
+		, p{new wrap<string>{v}}
+		{
+		}
+		value::value(string &&v)
+		: t{type::string}
+		, p{new wrap<string>{v}}
+		{
+		}
+		value::value(char const *v)
+		: t{type::string}
+		, p{new wrap<string>{v}}
+		{
+		}
+		value::value(array const &v)
+		: t{type::array}
+		, p{new wrap<array>{v}}
+		{
+		}
+		value::value(array &&v)
+		: t{type::array}
+		, p{new wrap<array>{v}}
+		{
+		}
+		value::value(object const &v)
+		: t{type::object}
+		, p{new wrap<object>{v}}
+		{
+		}
+		value::value(object &&v)
+		: t{type::object}
+		, p{new wrap<object>{v}}
+		{
+		}
+		value::value(value const &v)
+		: t{v.t}
+		, p{v.p? v.p->clone() : nullptr}
+		{
+		}
+		value &value::operator=(value const &v)
+		{
+			t = v.t;
+			p.reset(v.p? v.p->clone() : nullptr);
+			return *this;
+		}
+		value::~value() noexcept = default;
+
+		bool operator==(value const &a, value const &b) noexcept
+		{
+			return a.t == b.t
+			&&     a.t != type::null
+			&&     *a.p == *b.p;
+		}
+		bool operator<(value const &a, value const &b) noexcept
+		{
+			return json::operator</*work around VC++ bug*/(a.t, b.t)
+			||     (   a.t == b.t
+			        && a.t != type::null
+			        && *a.p < *b.p);
+		}
+
 		bool value::boolean_value() const
 		{
 			return dynamic_cast<wrap<bool> const &>(*p).v;
--- src/json.hpp
+++ src/json.hpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <string>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace LB
@@ -39,51 +40,15 @@ namespace LB
 			using object = std::multimap<string, value>;
 
 			value() noexcept = delete;
-			value(std::nullptr_t) noexcept
-			: t{type::null}
-			, p{nullptr}
-			{
-			}
-			value(bool v)
-			: t{type::boolean}
-			, p{new wrap<bool>{v}}
-			{
-			}
-			value(integer v)
-			: t{type::integer}
-			, p{new wrap<integer>{v}}
-			{
-			}
-			value(real v)
-			: t{type::real}
-			, p{new wrap<real>{v}}
-			{
-			}
-			value(string const &v)
-			: t{type::string}
-			, p{new wrap<string>{v}}
-			{
-			}
-			value(string &&v)
-			: t{type::string}
-			, p{new wrap<string>{v}}
-			{
-			}
-			value(char const *v)
-			: t{type::string}
-			, p{new wrap<string>{v}}
-			{
-			}
-			value(array const &v)
-			: t{type::array}
-			, p{new wrap<array>{v}}
-			{
-			}
-			value(array &&v)
-			: t{type::array}
-			, p{new wrap<array>{v}}
-			{
-			}
+			value(std::nullptr_t) noexcept;
+			value(bool v);
+			value(integer v);
+			value(real v);
+			value(string const &v);
+			value(string &&v);
+			value(char const *v);
+			value(array const &v);
+			value(array &&v);
 			template<typename ForwardIt>
 			value(ForwardIt begin, ForwardIt end)
 			: value(array{begin, end})
@@ -93,56 +58,33 @@ namespace LB
 			: value(array{v})
 			{
 			}
-			value(object const &v)
-			: t{type::object}
-			, p{new wrap<object>{v}}
-			{
-			}
-			value(object &&v)
-			: t{type::object}
-			, p{new wrap<object>{v}}
-			{
-			}
+			value(object const &v);
+			value(object &&v);
 			value(std::initializer_list<std::pair<string const, value>> v)
 			: value(object{v})
 			{
 			}
-			value(value const &v)
-			: t{v.t}
-			, p{v.p? v.p->clone() : nullptr}
-			{
-			}
+			value(value const &v);
 			value(value &&) noexcept = default;
-			value &operator=(value const &v)
-			{
-				t = v.t;
-				p.reset(v.p? v.p->clone() : nullptr);
-				return *this;
-			}
+			value &operator=(value const &v);
 			value &operator=(value &&) noexcept = default;
-			~value() noexcept = default;
+			~value() noexcept;
 
-			friend bool operator==(value const &a, value const &b) noexcept
-			{
-				return a.t == b.t && *a.p == *b.p;
-			}
-			friend bool operator<(value const &a, value const &b) noexcept
-			{
-				return json::operator</*work around VC++ bug*/(a.t, b.t) || (a.t == b.t && *a.p < *b.p);
-			}
+			friend bool operator==(value const &a, value const &b) noexcept;
+			friend bool operator<(value const &a, value const &b) noexcept;
 
 			operator type() const noexcept
 			{
 				return t;
 			}
 
-			bool           boolean_value() const;
+			bool           boolean_value() const  ;
 			bool    const &boolean_cref () const &;
 			bool          &boolean_ref  ()       &;
-			integer        integer_value() const;
+			integer        integer_value() const  ;
 			integer const &integer_cref () const &;
 			integer       &integer_ref  ()       &;
-			real           real_value   () const;
+			real           real_value   () const  ;
 			real    const &real_cref    () const &;
 			real          &real_ref     ()       &;
 			string  const &string_cref  () const &;
@@ -157,70 +99,10 @@ namespace LB
 
 		private:
 			type t;
-			struct base
-			{
-				virtual ~base() noexcept = default;
-
-				friend bool operator==(base const &a, base const &b) noexcept
-				{
-					return a.equals(b);
-				}
-				friend bool operator<(base const &a, base const &b) noexcept
-				{
-					return a.less(b);
-				}
-
-				virtual bool equals(base const &) const noexcept = 0;
-				virtual bool less(base const &) const noexcept = 0;
-				virtual base *clone() const noexcept = 0;
-			};
+			struct base;
 			std::unique_ptr<base> p;
 			template<typename T>
-			struct wrap final
-			: base
-			{
-				T v;
-				wrap() noexcept
-				: v{}
-				{
-				}
-				wrap(T const &t) noexcept
-				: v(t)
-				{
-				}
-				wrap(T &&t) noexcept
-				: v(t)
-				{
-				}
-				wrap(wrap const &w) noexcept
-				: v(w.v)
-				{
-				}
-				wrap(wrap &&) noexcept = default;
-				wrap &operator=(wrap const &) = delete;
-				wrap &operator=(wrap &&) = delete;
-
-				virtual bool equals(base const &p) const noexcept override
-				{
-					if(auto w = dynamic_cast<wrap const *>(&p))
-					{
-						return v == w->v;
-					}
-					return false;
-				}
-				virtual bool less(base const &p) const noexcept override
-				{
-					if(auto w = dynamic_cast<wrap const *>(&p))
-					{
-						return v < w->v;
-					}
-					return false;
-				}
-				virtual wrap *clone() const noexcept override
-				{
-					return new wrap{*this};
-				}
-			};
+			struct wrap;
 
 			friend void serialize(value const &v, string &s, std::size_t depth);
 		};
