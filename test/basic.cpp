d034a3572f197a40d05b9987a62398298c8c7bff
Nicholas "LB" Braden <LB@LB-Stuff.com>
2016-05-28 01:48:12 -0500

    Expand basic test

--- test/basic.cpp
+++ test/basic.cpp
@@ -6,5 +6,15 @@ namespace json = LB::json;
 
 int main()
 {
-	std::cout << serialize(json::value(json::array{true, nullptr, "test", json::integer{7}})) << std::endl;
+	json::value v
+	(
+		json::object
+		{
+			{"test", json::array{nullptr, true, json::integer{7}, json::real{3.141592654}, "test", json::array{}, json::object{}}},
+			{"test", nullptr}
+		}
+	);
+	std::cout
+		<< serialize(v) << std::endl
+		<< serialize(v, {json::serialize_formatting::pretty}) << std::endl;
 }
